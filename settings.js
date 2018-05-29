ad0d35b84440b4cbfb5c9a85573538a87d7d5810
Tim Filer <tim.j.filer@gmail.com>
2018-05-29 15:19:49 -0500

    updated listen port

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ exports.dbConfig = {
 	port: 1433
 };
 
-exports.webPort = 3000;
+exports.webPort = 3001;
 exports.httpMsgsFormat = "JSON";
 
 exports.memberSql = "SELECT m.[Member ID] as memberId, m.First_Name as firstName, m.Last_Name as lastName, m.middle_name as middleName, m.degrees as degree, m.title as academicTitle, m.dept as department, m.org as institution, m.bus_phone as busPhone, m.email, m.address, m.address_2 as address2, m.city, m.state, m.zip, m.country, m.fax as fax, t.description as memberType, m.dues_year as duesYear FROM Main m INNER JOIN [Member Types] t ON m.membertype = t.memtype ";
